4314717763f53536bd4e9611d381b7078c466e81
xenrath <xenrath89@gmail.com>
2023-09-13 05:14:18 +0700

    done

--- app/Http/Livewire/ContactIndex.php
+++ app/Http/Livewire/ContactIndex.php
@@ -4,19 +4,34 @@ namespace App\Http\Livewire;
 
 use App\Contact;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class ContactIndex extends Component
 {
+    use WithPagination;
+
     public $statusUpdate = false;
+    public $paginate = 5;
+    public $search;
 
     protected $listeners = [
-        'contactStored' => 'handleStored'
+        'contactStored' => 'handleStored',
+        'contactUpdated' => 'handleUpdated',
     ];
+
+    protected $updatesQueryString = ['search'];
+
+    public function mount()
+    {
+        $this->search = request()->query('search', $this->search);
+    }
 
     public function render()
     {
         return view('livewire.contact-index', [
-            'contacts' => Contact::latest()->get()
+            'contacts' => $this->search === null ?
+                Contact::latest()->paginate($this->paginate) :
+                Contact::latest()->where('name', 'like', '%' . $this->search . '%')->paginate($this->paginate)
         ]);
     }
 
@@ -28,8 +43,24 @@ class ContactIndex extends Component
         $this->emit('getContact', $contact);
     }
 
+    public function destroy($id)
+    {
+        if ($id) {
+            $contact = Contact::find($id);
+            $contact->delete();
+
+            session()->flash('message', 'Contact was deleted!');
+        }
+    }
+
     public function handleStored($contact)
     {
         session()->flash('message', 'Contact ' . $contact['name'] . ' was stored!');
+    }
+
+    public function handleUpdated($contact)
+    {
+        $this->statusUpdate = false;
+        session()->flash('message', 'Contact ' . $contact['name'] . ' was updated!');
     }
 }
--- resources/views/livewire/contact-index.blade.php
+++ resources/views/livewire/contact-index.blade.php
@@ -9,7 +9,24 @@
   @else
     <livewire:contact-create></livewire:contact-create>
   @endif
+  
   <hr>
+  
+  <div class="row">
+    <div class="col">
+      <select wire:model="paginate" class="form-control form-control-sm w-auto">
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+      </select>
+    </div>
+    <div class="col">
+      <input wire:model="search" type="text" class="form-control form-control-sm" placeholder="Search..">
+    </div>
+  </div>
+
+  <hr>
+  
   <table class="table">
     <thead class="thead-dark">
       <tr>
@@ -30,10 +47,11 @@
           <td>{{ $contact->phone }}</td>
           <td>
             <button wire:click="getContact({{ $contact->id }})" class="btn btn-sm btn-info text-white">Edit</button>
-            <button class="btn btn-sm btn-danger text-white">Delete</button>
+            <button wire:click="destroy({{ $contact->id }})" class="btn btn-sm btn-danger text-white">Delete</button>
           </td>
         </tr>
       @endforeach
     </tbody>
   </table>
+  {{ $contacts->links() }}
 </div>
